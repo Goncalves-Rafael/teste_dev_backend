5a77e17d6360e8db0aeb0986ceb5fc42358851f1
Rafael Oliveira <rafael-goncalves-oliveira@outlook.com>
2022-12-05 13:52:13 -0300

    [Challenge 2] Fix smoke test

--- desafio_2/src/test/java/com/conexa/test/dev/backend/desafio_2/TesteDevBackendDesafio2ApplicationTests.java
+++ desafio_2/src/test/java/com/conexa/test/dev/backend/desafio_2/TesteDevBackendDesafio2ApplicationTests.java
@@ -2,8 +2,10 @@ package com.conexa.test.dev.backend.desafio_2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles(profiles = "test")
 class TesteDevBackendDesafio2ApplicationTests {
 
 	@Test
